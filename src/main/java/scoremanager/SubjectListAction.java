4630a1b07a795ca2e5b6f6f2bf57f79651e26245
m_takata <m_takata@10.251.242.159>
2026-04-23 12:27:44 +0900

    q

--- src/main/java/scoremanager/SubjectListAction.java
+++ src/main/java/scoremanager/SubjectListAction.java
@@ -9,13 +9,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import tool.Action;
 
 public class SubjectListAction extends Action {
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
         SubjectDAO dao = new SubjectDAO();
+        
         List<Subject> list = dao.search();
 
         request.setAttribute("subjects", list);
 
-        return "subject_list.jsp";
+        return "subject_list.jsp"; 
     }
 }
